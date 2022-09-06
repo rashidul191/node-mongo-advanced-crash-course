44cbb25bc238c1928ea4587240ff33597070fd76
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-09-06 22:27:02 +0600

    create repo for node mongo advanced crash course

---
+++ Module-1 JavaScript For BackEnd Development/1.1_SynchronousAsynchronous.js
@@ -0,0 +1,18 @@
+console.log("Welcome to ACC ");
+
+setTimeout(() => {
+  console.log("Use Set Time Out");
+  setTimeout(() => {
+    console.log("More Use Set time out");
+  }, 500);
+}, 100);
+
+console.log("Rashidul");
+console.log("a");
+console.log("b");
+console.log("c");
+console.log("d");
+console.log("e");
+console.log("f");
+console.log("g");
+console.log("h");
